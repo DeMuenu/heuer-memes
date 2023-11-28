5abffdaf8cf1576c17449fa391ca94261b661615
DeMuenu <jannisa@gmx.ch>
2023-11-29 00:04:28 +0100

    works

--- test.py
+++ test.py
@@ -1,19 +1,12 @@
-import fetch, JSON
-import asyncio
+import pickle
 
-async def load_post():
-    print("sent")
-    post_content = Element('password').element.value
-    response = await fetch('http://127.0.0.1:5000/post', {
-        'method': 'POST',
-        'headers': {'Content-Type': 'application/json'},
-        'body': JSON.stringify({'content': post_content})
-    })
+tweets = []
 
-    
-    
-    new_post = f'<div><p>{post_content}</p></div>'
-    Element('posts').element.innerHTML += new_post
-    Element('password').element.value = ''  # Clear the input field
-    print("no Responce")
-    print(response)
+with open('tweets.pkl', 'wb') as file:
+    pickle.dump(tweets, file)
+
+
+with open('tweets.pkl', 'rb') as file:
+    loaded_list = pickle.load(file)
+
+print(loaded_list)
