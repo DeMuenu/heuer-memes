3e5d13d4d211f7d5b5e0fab5a63250271d7bb00e
DeMuenu <96650288+DeMuenu@users.noreply.github.com>
2023-11-29 14:20:47 +0100

    mm

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ endungen = ["Master", "Krieger", "Magier", "Baron", "Herrscher", "Fortnite Enjoy
 app = Flask(__name__)
 CORS(app)
 passwordGlobal = "test"
+viewPass = "view"
 # Dummy storage for tweets
 tweets = []
 
@@ -33,6 +34,31 @@ def handle_post_tweet():
 
 @app.route('/get_tweets', methods=['GET'])
 def handle_get_tweets():
+    auth_header = request.headers.get('Authorization')
+    if auth_header and (auth_header == passwordGlobal or auth_header == viewPass):
+        return jsonify(tweets), 200
+    else:
+        return jsonify({'message': 'Unauthorized'}), 401
+    
+
+
+@app.route('/post_review', methods=['POST'])
+def handle_post_review():
+    data = request.json
+    password = data.get('password')
+    if password == passwordGlobal:
+        anfang = random.choice(anfaenge)
+        ende = random.choice(endungen)
+        name = f"{anfang} {ende}"
+        tweets.append({'title': data['tweetTitle'], 'text': data['tweet'], 'by': name})
+        with open('tweets.pkl', 'wb') as file:
+            pickle.dump(tweets, file)
+        return jsonify({'message': 'Tweet posted successfully'}), 200
+    else:
+        return jsonify({'message': 'Unauthorized'}), 401
+
+@app.route('/get_reviews', methods=['GET'])
+def handle_get_review():
     auth_header = request.headers.get('Authorization')
     if auth_header and auth_header == passwordGlobal:
         return jsonify(tweets), 200
