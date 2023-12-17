571243774dbcd29365139a565cd69a6f295d441c
DeMuenu <jannisa@gmx.ch>
2023-12-17 16:44:20 +0100

    comments work

--- old servers/1.py
+++ old servers/1.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import random
 import pickle
 import os
-import time    
+import time
 
 
 anfaenge = ["Gagi des", "Furz vom", "Kinder des", "Penis des", "Arschhaar des", "Theodor der", "Heuer der"]
@@ -20,19 +20,9 @@ tweets = []
 rewiews = []
 
 
-try:
-    with open('tweets.pkl', 'rb') as file:
-        if file:
-            tweets = pickle.load(file)
-except:
-    print("no tweets yet")
-
-try:
-    with open('rewiews.pkl', 'rb') as file:
-        if file:
-            rewiews = pickle.load(file)
-except:
-    print("no reviews yet")
+with open('tweets.pkl', 'rb') as file:
+    if file:
+        tweets = pickle.load(file)
 
 @app.route('/post_tweet', methods=['POST'])
 def handle_post_tweet():
@@ -77,8 +67,6 @@ def handle_post_review():
             file.save(f"{pahtHTTPS}{filename}")
             
             rewiews.append({"filename" : filename, "reviews" : []})
-            with open('rewiews.pkl', 'wb') as file:
-                pickle.dump(rewiews, file)
             return jsonify(rewiews), 200
         else:
             return jsonify({'message': 'Unauthorized'}), 401
@@ -89,36 +77,7 @@ def handle_get_review():
 #    if auth_header and auth_header == passwordGlobal:
     return jsonify(rewiews), 200
 
-@app.route('/post_comment', methods=['POST'])
-def handle_post_comment():
-    print("hah")
-    data = request.json
-    password = data.get('password')
-    if password == passwordGlobal:
-        anfang = random.choice(anfaenge)
-        ende = random.choice(endungen)
-        name = f"{anfang} {ende}"
-        filename = data["tweetTitle"]
-        print(filename)
-        filename = filename.replace("_", ".")
-        filename = filename + ".png"
-        for t in rewiews:
-            print(t)
-
-            print(filename)
-            if t["filename"] == filename:
-                t["reviews"].append(data["tweet"])
-                with open('rewiews.pkl', 'wb') as file:
-                    pickle.dump(rewiews, file)
-
-        #with open('tweets.pkl', 'wb') as file:
-        #    pickle.dump(tweets, file)
-        return jsonify({'message': 'Tweet posted successfully'}), 200
-    else:
-        return jsonify({'message': 'Unauthorized'}), 401
-
    
 
 if __name__ == '__main__':
-    #app.run(debug=True)#host='0.0.0.0', port=5000, ssl_context=('/etc/letsencrypt/live/vps2441966.servdiscount-customer.com/fullchain.pem', '/etc/letsencrypt/live/vps2441966.servdiscount-customer.com/privkey.pem'))
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('/etc/letsencrypt/live/vps2441966.servdiscount-customer.com/fullchain.pem', '/etc/letsencrypt/live/vps2441966.servdiscount-customer.com/privkey.pem'))
+    app.run(host='0.0.0.0', port=5000, ssl_context=('/etc/letsencrypt/live/vps2441966.servdiscount-customer.com/fullchain.pem', '/etc/letsencrypt/live/vps2441966.servdiscount-customer.com/privkey.pem'))
